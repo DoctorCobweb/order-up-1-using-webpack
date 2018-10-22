0cf9eee7ea3e61ae8c1a10c2603b4a8f5be0fe83
dre <spinninghalf@gmail.com>
2018-10-22 16:05:23 +1100

    normalized db. first apparent working version

--- src/main-process/knuckle-dragger/db-handler.js
+++ src/main-process/knuckle-dragger/db-handler.js
@@ -33,33 +33,23 @@ export const insertSingleOrder = (order) => {
       return r.db('orderUp').table('orders').insert(orderDoc).run(conn)
     })
     .then(orderRes => {
-      console.log('orderRes')
-      console.log(orderRes)
+      // console.log('orderRes')
+      // console.log(orderRes)
       const orderKey = orderRes.generated_keys[0]
       const courseDocs = Object.keys(order.meals).map(course => ({
         name: course,
         order_id: orderKey
       }))
+
       console.log('courseDocs')
       console.log(courseDocs)
+
       return r.db('orderUp').table('courses').insert(courseDocs).run(conn)
     })
     .then(courseDocsRes => {
-      console.log('courseDocsRes')
-      console.log(courseDocsRes)
-
+      // console.log('courseDocsRes')
+      // console.log(courseDocsRes)
       const zippedCoursesWithKeys = _.zip(courseDocsRes.generated_keys, _.values(order.meals))
-      console.log('zippedCoursesWithKeys')
-      console.log(JSON.stringify(zippedCoursesWithKeys))
-
-      // const items = Object.keys(order.meals).map(course => {
-      //   order.meals[course].map(item => ({
-      //     name: item.name,
-      //     quantity: item.quantity
-      //     course_id: "blahblah"
-      //   }))
-      // })
-
       const itemDocs = _.flatten(zippedCoursesWithKeys.map(zippedCourse => {
         const [ courseKey, courseItems ] = zippedCourse
         return courseItems.map(item => ({
@@ -73,6 +63,64 @@ export const insertSingleOrder = (order) => {
       console.log(itemDocs)
 
       return r.db('orderUp').table('items').insert(itemDocs).run(conn)
+    })
+    .then(itemDocsRes => {
+      // console.log('itemDocsRes')
+      // console.log(itemDocsRes)
+
+      // [ [{entree_item},{entree_item}], [{mains_item},{mains_item}], ...]
+      const eachCoursesItems = _.values(order.meals)
+
+      // [ {entree_item},{entree_item}, {mains_item},{mains_item}, ...]
+      const flatItems = _.flatten(eachCoursesItems)
+
+      // [[item_key, {entree_item}], [item_key, {entree_item}], [item_key,{mains_item}], ...]
+      const zipped = _.zip(itemDocsRes.generated_keys, flatItems)
+
+      const itemInfos = _.flatten(zipped.map(zippedItem => {
+        const [ itemKey, item ] = zippedItem
+        return item.info.map(info => ({
+          item_id: itemKey
+        }))
+      }))
+
+      console.log('itemInfos')
+      console.log(itemInfos)
+
+      return r.db('orderUp').table('infos').insert(itemInfos).run(conn)
+    })
+    .then(infoDocsRes => {
+      // console.log('infoDocsRes')
+      // console.log(infoDocsRes)
+
+      // [{item}, {item}, ...]
+      const allItems = _.flatten(_.values(order.meals))
+
+      // [ [info], [info], ... ]
+      const allInfos = _.flatten(allItems.map(item => item.info))
+
+      // console.log('allInfos')
+      // console.log(allInfos)
+
+      // [ [ info_key, {itemInfo: [{info:'...', quantity: ''}]} ], [...], ... ]
+      const allInfosZipped = _.zip(infoDocsRes.generated_keys, allInfos)
+      const allInfoLines = _.flatten(allInfosZipped.map( elem => {
+        const [ infoKey, itemInfoObj ] = elem
+        return itemInfoObj.itemInfo.map(singleItemInfoLine => ({
+          info_id: infoKey,
+          name: singleItemInfoLine.info,
+          quatity: singleItemInfoLine.quantity
+        }))
+      }))
+
+      console.log('allInfoLines')
+      console.log(JSON.stringify(allInfoLines, null, 2))
+
+      return r.db('orderUp').table('infoLines').insert(allInfoLines).run(conn)
+    })
+    .then(infoLinesRes => {
+      console.log('infoLinesRes')
+      console.log(infoLinesRes)
     })
     .catch(err => {
       if (err) throw err
