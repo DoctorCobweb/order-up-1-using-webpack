0662bedee83f7bf059dd2c606aaaad8fd428c839
dre <spinninghalf@gmail.com>
2018-12-03 17:47:08 +1100

    wip: add green/red dots to hold/call-away and display time since called away

--- app/renderer-process/appOne/components/BoardListItem.js
+++ app/renderer-process/appOne/components/BoardListItem.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import moment from 'moment'
 import { sortCoursesInOrder } from '../../../shared/selectors/lists'
+import greenDot from '../assets/green-dot.png'
+import redDot from '../assets/red-dot.png'
 
 class BoardListItem extends React.Component {
   state = {
@@ -69,59 +71,110 @@ class BoardListItem extends React.Component {
   )
 }
 
-const Course = (props) => (
-  <div
-    className={ props.order.location === "RESTAURANT BAR" ?
-      "board-list__course border-red"
-      :
-      props.order.location === "GAMING BAR" ?
-      "board-list__course border-green"
-      :
-      "board-list__course border-blue"
-    }
-  >
-    <h3
-      className={ props.course.items.every( item => item.quantity === 0 ) ? "board-list__course-name": "" }
+class Course extends React.Component {
+  state = {
+    minutesAway: 0,
+  }
+
+  render = () => (
+    <div
+      className={ this.props.order.location === "RESTAURANT BAR" ?
+        "board-list__course border-red"
+        :
+        this.props.order.location === "GAMING BAR" ?
+        "board-list__course border-green"
+        :
+        "board-list__course border-blue"
+      }
     >
-      { props.course.name }
-      { 
-        ( 
-          props.course.name === 'MAINS DINNER' &&
-          props.order.goOnMains && 
-          !!props.order.goOnMainsStartedAt
-        ) ?
-          ` (AWAY @${moment(props.order.goOnMainsStartedAt).format("HH:mm")})`
-            :
-          ''
+      <h3
+        className={ 
+          this.props.course.items.every( item => item.quantity === 0 )
+          ?
+            "board-list__course-name-hide"
+          :
+            "board-list__course-name__container"
+        }
+      >
+        { this.props.course.name }
+        { 
+          ( 
+            this.props.course.name === 'MAINS DINNER' &&
+            this.props.order.goOnMains && 
+            !!this.props.order.goOnMainsStartedAt
+          ) ?
+            <div className="away-on-mains__container">
+              <img
+                className="away-on-mains"
+                src={ greenDot }
+                alt="away"
+              />
+              <div>
+                { moment(this.props.order.goOnMainsStartedAt).format("HH:mm") }
+              </div>
+              <div>
+               { setInterval(() => {
+                 this.setState(prevState => ({
+                   minutesAway: prevState.minutesAway + 1,
+                 }))
+               }, 1000)}
+              </div>
+              <div>
+                { this.state.minutesAway }
+              </div>
+            </div>
+              :
+            ''
+        }
+        { 
+          this.props.course.name === 'MAINS DINNER' &&  !this.props.order.goOnMains ?
+            <img
+              className="hold-mains"
+              src={ redDot }
+              alt="hold"
+            />
+              :
+            ''
+        }
+        { 
+          (
+            this.props.course.name === 'BAR MEALS' &&
+            this.props.order.goOnMains &&
+            !!this.props.order.goOnMainsStartedAt
+          ) ?
+            <div className="away-on-mains__container">
+              <img
+                className="away-on-mains"
+                src={ greenDot }
+                alt="away"
+              />
+              <div>
+                { moment(this.props.order.goOnMainsStartedAt).format("HH:mm") }
+              </div>
+              <div>
+                ((( { moment().diff(moment(this.props.order.gonOnMainsStartedAt), 'minutes') } )))
+              </div>
+            </div>
+              :
+            ''
+        }
+        { 
+          this.props.course.name === 'BAR MEALS' &&  !this.props.order.goOnMains ?
+            <img
+              className="hold-mains"
+              src={ redDot }
+              alt="hold"
+              />
+              :
+            ''
+        }
+      </h3>
+      {
+        this.props.course.items.map(item => <Item key={ item._id } item={ item }/>)
       }
-      { 
-        props.course.name === 'MAINS DINNER' &&  !props.order.goOnMains ?
-          ` (HOLD)`
-            :
-          ''
-      }
-      { 
-        (
-          props.course.name === 'BAR MEALS' &&
-          props.order.goOnMains &&
-          !!props.order.goOnMainsStartedAt
-        ) ?
-          ` (AWAY @${moment(props.order.goOnMainsStartedAt).format("HH:mm")})`
-            :
-          ''
-      }
-      { 
-        props.course.name === 'BAR MEALS' &&  !props.order.goOnMains ?
-          `(HOLD)`
-            :
-          ''
-      }
-    </h3>
-    {
-      props.course.items.map(item => <Item key={ item._id } item={ item }/>)
-    }
-  </div>
-)
+    </div>
+  )
+}
 
 const Item = (props) => (
   <div className={ props.item.quantity === 0 ? "board-list__item-container-done" : ""}>
